5d54abd23daa70f714a4d239e1a8cea7112b266e
Adrian Messado <adrian.messado@gmail.com>
2018-06-08 12:39:32 -0700

    create plans component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { PlansComponent } from './plans/plans.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    PlansComponent
   ],
   imports: [
     BrowserModule
